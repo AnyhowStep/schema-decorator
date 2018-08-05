594ec5fb30ce05898ba7c99e49d45032f12605b4
AnyhowStep <anyhowstep@hotmail.com>
2018-08-04 23:35:31 -0400

    Added isOptional(), isNullable(), isMaybe(), isOfType()

--- dist/src/main/check.js
+++ dist/src/main/check.js
@@ -6,4 +6,53 @@ function check(f, name, mixed) {
     return d(name, mixed);
 }
 exports.check = check;
+function isOptional(assert) {
+    const d = types_1.toAssertDelegateExact(assert);
+    try {
+        d("test", undefined);
+        return true;
+    }
+    catch (_err) {
+        //Tried to pass undefined, got error, does not allow undefined
+        return false;
+    }
+}
+exports.isOptional = isOptional;
+function isNullable(assert) {
+    const d = types_1.toAssertDelegateExact(assert);
+    try {
+        d("test", null);
+        return true;
+    }
+    catch (_err) {
+        //Tried to pass null, got error, does not allow null
+        return false;
+    }
+}
+exports.isNullable = isNullable;
+function isMaybe(assert) {
+    const d = types_1.toAssertDelegateExact(assert);
+    try {
+        d("test", undefined);
+        d("test", null);
+        return true;
+    }
+    catch (_err) {
+        //Tried to pass undefined|null, got error, does not allow undefined|null
+        return false;
+    }
+}
+exports.isMaybe = isMaybe;
+function isOfType(assert, mixed) {
+    const d = types_1.toAssertDelegateExact(assert);
+    try {
+        d("test", mixed);
+        return true;
+    }
+    catch (_err) {
+        //Tried to pass mixed, got error, mixed is invalid
+        return false;
+    }
+}
+exports.isOfType = isOfType;
 //# sourceMappingURL=check.js.map
