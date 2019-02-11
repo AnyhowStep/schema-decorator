f63a3f2d50bb73c46eb1e5d593598c95c9470d25
AnyhowStep <anyhowstep@hotmail.com>
2019-02-11 12:24:41 -0500

    Added more helper types

--- src/main/assert-lib/missing-value.ts
+++ src/main/assert-lib/missing-value.ts
@@ -1,25 +1,50 @@
 import {
     AnyAssertFunc,
-    toAssertDelegateExact
+    toAssertDelegateExact,
+    AssertDelegate,
+    TypeOf,
+    AcceptsOf,
+    CanAcceptOf,
 } from "../types";
 import {or} from "./operator";
 import {literal, excludeLiteral} from "./basic";
 
-export function optional<F extends AnyAssertFunc> (assert : F) {
+export type Optional<F extends AnyAssertFunc> = (
+    AssertDelegate<TypeOf<F>|undefined> &
+    {
+        __accepts : AcceptsOf<F>|undefined,
+        __canAccept : CanAcceptOf<F>|undefined,
+    }
+);
+export function optional<F extends AnyAssertFunc> (assert : F) : Optional<F> {
     const assertDelegate = toAssertDelegateExact(assert);
     return or(
         literal(undefined),
         assertDelegate
     );
 }
-export function nullable<F extends AnyAssertFunc> (assert : F) {
+export type Nullable<F extends AnyAssertFunc> = (
+    AssertDelegate<TypeOf<F>|null> &
+    {
+        __accepts : AcceptsOf<F>|null,
+        __canAccept : CanAcceptOf<F>|null,
+    }
+);
+export function nullable<F extends AnyAssertFunc> (assert : F) : Nullable<F> {
     const assertDelegate = toAssertDelegateExact(assert);
     return or(
         literal(null),
         assertDelegate
     );
 }
-export function maybe<F extends AnyAssertFunc> (assert : F) {
+export type Maybe<F extends AnyAssertFunc> = (
+    AssertDelegate<TypeOf<F>|null|undefined> &
+    {
+        __accepts : AcceptsOf<F>|null|undefined,
+        __canAccept : CanAcceptOf<F>|null|undefined,
+    }
+);
+export function maybe<F extends AnyAssertFunc> (assert : F) : Maybe<F> {
     const assertDelegate = toAssertDelegateExact(assert);
     return or(
         literal(undefined, null),
