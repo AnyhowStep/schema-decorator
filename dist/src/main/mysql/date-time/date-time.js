ba3c64e0252a9ba7c0bbcedf215140370ca66a54
AnyhowStep <anyhowstep@hotmail.com>
2019-03-01 12:02:33 -0500

    Fixed mysql.dateTime() allowing invalid Date objects

--- dist/src/main/mysql/date-time/date-time.js
+++ dist/src/main/mysql/date-time/date-time.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sd = require("../../assert-lib");
 const DateTimeUtil = require("./util");
 function buildDateTimeDelegate(fractionalSecondPrecision /*|4|5|6*/) {
-    return sd.or(sd.instanceOf(Date), sd.chain(sd.string(), (name, str) => {
+    return sd.or(sd.chain(sd.string(), (name, str) => {
         try {
             return DateTimeUtil.fromSqlUtc(str, fractionalSecondPrecision);
         }
@@ -12,7 +12,28 @@ function buildDateTimeDelegate(fractionalSecondPrecision /*|4|5|6*/) {
         }
     }), 
     //To work with JSON serialization
-    sd.chain(sd.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?Z$/, name => `${name} must be in the format YYYY-MM-DDTHH:mm:ss.sssZ`), (name, str) => {
+    sd.chain(sd.or(sd.string(), 
+    /*
+        We turn the Date object into its JSON representation
+        because the Date object may have a millisecond part
+        when we do not allow it.
+
+        For example,
+        new Date().toJSON() //2019-01-01T00:00:00.123Z
+
+        The above is not assignable to DATETIME(0)
+        But is assignable to DATETIME(3)
+
+        -----
+
+        new Date().toJSON() //2019-01-01T00:00:00.120Z
+
+        The above is not assignable to DATETIME(0)
+        But is assignable to DATETIME(2) or DATETIME(3)
+    */
+    sd.chain(sd.instanceOf(Date), (_name, d) => {
+        return d.toJSON();
+    })), sd.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?Z$/, name => `${name} must be in the format YYYY-MM-DDTHH:mm:ss.sssZ`), (name, str) => {
         try {
             str = str.replace("T", " ").replace("Z", "");
             return DateTimeUtil.fromSqlUtc(str, fractionalSecondPrecision);
--- src/main/mysql/date-time/date-time.ts
+++ src/main/mysql/date-time/date-time.ts
@@ -9,7 +9,6 @@ function buildDateTimeDelegate (
     fractionalSecondPrecision : 0|1|2|3/*|4|5|6*/
 ) {
     return sd.or(
-        sd.instanceOf(Date),
         sd.chain(
             sd.string(),
             (name : string, str : string) => {
@@ -22,6 +21,33 @@ function buildDateTimeDelegate (
         ),
         //To work with JSON serialization
         sd.chain(
+            sd.or(
+                sd.string(),
+                /*
+                    We turn the Date object into its JSON representation
+                    because the Date object may have a millisecond part
+                    when we do not allow it.
+
+                    For example,
+                    new Date().toJSON() //2019-01-01T00:00:00.123Z
+
+                    The above is not assignable to DATETIME(0)
+                    But is assignable to DATETIME(3)
+
+                    -----
+
+                    new Date().toJSON() //2019-01-01T00:00:00.120Z
+
+                    The above is not assignable to DATETIME(0)
+                    But is assignable to DATETIME(2) or DATETIME(3)
+                */
+                sd.chain(
+                    sd.instanceOf(Date),
+                    (_name : string, d : Date) => {
+                        return d.toJSON();
+                    }
+                )
+            ),
             sd.match(
                 /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,3})?Z$/,
                 name => `${name} must be in the format YYYY-MM-DDTHH:mm:ss.sssZ`
